b06fc7ddeb3616f5d4c699ebac28bafae086c885
HyeonggyuKang <kin0204@naver.com>
2025-10-17 12:05:11 +0900

    js 에서 HTML 선택자 사용

--- 02_javascript_es6/03_distructuring-assignment/02_object-distructuring-assignment/03_function-parameter.js
+++ 02_javascript_es6/03_distructuring-assignment/02_object-distructuring-assignment/03_function-parameter.js
@@ -5,20 +5,30 @@ let exampleProduct = {
 
 function displayProduct(obj){
     let {items, producer} = obj;
+    items.push('test');
 
     console.log('items:', items);       // items: [ 'Coffee', 'Donut' ]
     console.log('producer:', producer); // producer: 신사임당
 
-    console.log(items === obj.items);
+    console.log('아이템 확인:', items === obj.items);
+    console.log('생산자확인:', producer === obj.producer);
     producer = 'asd';
 };
+let {items:it, producer:a} = exampleProduct;
+it.push('aaaa');
+a = 'asdasdas';
+console.log('-------------------------')
+console.log(exampleProduct);
+console.log('-------------------------')
 
 /* 객체를 자동 으로 객체 구조 분해 할당이 됨 */
 function displayProduct2({producer, items, brnad = '엔코아'}){
+    console.log('------------------------display2--------------------------')
+    items.push('123');
+    producer = 'Kang';
     console.log('items:', items);       // items: [ 'Coffee', 'Donut' ]
     console.log('producer:', producer); // producer: 신사임당
     console.log('brand:', brnad);       // brand: 엔코아
-    producer = 'Kang';
 };
 
 
